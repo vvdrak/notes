5aa8bcb973f1ba539f8cd39039dd22117876a4ab
vva2 <vva2@gelicon.biz>
2020-07-16 18:35:29 +0500

    initial

---
+++ app/src/main/java/ru/gross/sample/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.gross.sample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gross.sample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
