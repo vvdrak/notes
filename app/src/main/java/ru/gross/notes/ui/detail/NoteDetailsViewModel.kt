a06a79a0374d9020c3e8dbcf4c109a4fbfb45155
Vladimir Gross <vva2@gelicon.biz>
2022-09-06 19:18:07 +0500

    increment versions and resolve errors

--- app/src/main/java/ru/gross/notes/ui/detail/NoteDetailsViewModel.kt
+++ app/src/main/java/ru/gross/notes/ui/detail/NoteDetailsViewModel.kt
@@ -65,7 +65,7 @@ class NoteDetailsViewModel constructor(
     ) : ViewModelProvider.Factory {
 
         @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return NoteDetailsViewModel(
                 noteId,
                 noteDetailMapper,
